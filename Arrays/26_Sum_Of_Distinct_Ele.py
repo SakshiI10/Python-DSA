f5a0d458073621853683dd3a872f253e1a41f766
SakshiI10 <iamsakshiingole@gmail.com>
2024-07-18 09:04:15 +0530

    File added

---
+++ Arrays/26_Sum_Of_Distinct_Ele.py
@@ -0,0 +1,26 @@
+'''
+You are given an array Arr of size N. Find the sum of distinct elements in an array.
+
+Input:
+N = 5
+Arr[] = {1, 2, 3, 4, 5}
+Output: 15
+'''
+
+class Solution:
+    def findSum(self, arr, n):
+        unique_elements = set()
+        sum = 0
+        
+        for i in range(n):
+            if arr[i] not in unique_elements:  
+                unique_elements.add(arr[i])  
+                sum += arr[i]  
+
+        return sum
+
+sol = Solution()
+N = 5
+Arr = [5, 5, 5, 5, 5]
+print(sol.findSum(Arr, N))  # Output: 15
+
