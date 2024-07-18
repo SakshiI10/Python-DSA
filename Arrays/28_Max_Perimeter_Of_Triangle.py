5b6ea3554df05884058c4328001504f94c0d883e
SakshiI10 <iamsakshiingole@gmail.com>
2024-07-18 09:17:27 +0530

    File added

---
+++ Arrays/28_Max_Perimeter_Of_Triangle.py
@@ -0,0 +1,21 @@
+'''
+Given an Array of non-negative integers. Find out the maximum perimeter of the triangle from the array.
+
+Input : arr[ ] = {6, 1, 6, 5, 8, 4}
+Output : 20
+'''
+
+class Solution():
+    def maxPerimeter (self, arr, n) : 
+        arr.sort()
+        
+        for i in range(n - 1, 1, -1):
+            if arr[i - 2] + arr[i - 1] > arr[i]:
+                return arr[i - 2] + arr[i - 1] + arr[i]
+        
+        return -1
+
+sol=Solution()
+arr = [6, 1, 6, 5, 8, 4]
+n=len(arr)
+print(sol.maxPerimeter(arr, n))
