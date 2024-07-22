bae256bb39eb90cbabbbe7bdc22dcd8f363d55c6
SakshiI10 <iamsakshiingole@gmail.com>
2024-07-22 23:08:02 +0530

    File added

---
+++ Arrays/27_A_guy_with_mental_problem.py
@@ -0,0 +1,31 @@
+'''
+A guy has to reach his home and does not want to be late. He takes train to reach home. He has a mental illness, so he always switches train at every station.
+Input: N = 3
+A[] = {2, 1, 2}
+B[] = {3, 2, 1}
+Output: 5'''
+
+class Solution:
+    def minTime(self, a, b, n):
+        time_with_A = 0
+        time_with_B = 0
+
+        for i in range(n):
+            if i % 2 == 0:
+                time_with_A += a[i]
+            else:
+                time_with_A += b[i]
+
+        for i in range(n):
+            if i % 2 == 0:
+                time_with_B += b[i]
+            else:
+                time_with_B += a[i]
+
+        return min(time_with_A, time_with_B)
+
+solution = Solution()
+a = [2, 1, 2]
+b = [3, 2, 1]
+n = 3
+print(solution.minTime(a, b, n))  # Output: 5
--- Strings/33_Reverse_a_str_with_spaces_intact.py
+++
@@ -1,7 +0,0 @@
-'''
-Given a string, your task is to reverse the string keeping the spaces intact to their positions.
-
-Input:
-S = "Help others"
-Output: sreh topleH'''
-
