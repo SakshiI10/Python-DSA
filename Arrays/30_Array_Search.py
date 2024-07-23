e2b4627bcc461c759b93c9f505c1e5621a1fff0f
SakshiI10 <iamsakshiingole@gmail.com>
2024-07-24 00:09:45 +0530

    File added

---
+++ Arrays/30_Array_Search.py
@@ -0,0 +1,23 @@
+'''
+Given an array, arr of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
+
+Input: arr[] = [1, 2, 3, 4], x = 3
+Output: 2
+
+Input: arr[] = [10, 8, 30, 4, 5], x = 5
+Output: 4
+
+Input: arr[] = [10, 8, 30], x = 6
+Output: -1'''
+
+class Solution:
+    def search(self, arr, x):
+        for i in range(len(arr)):
+            if arr[i] == x:
+                return i
+        return -1
+
+solution = Solution()
+print(solution.search([1, 2, 3, 4], 3))  # Output: 2
+print(solution.search([10, 8, 30, 4, 5], 5))  # Output: 4
+print(solution.search([10, 8, 30], 6))  # Output: -1
