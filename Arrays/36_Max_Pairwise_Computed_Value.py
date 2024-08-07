f297af927d08514fcdab9403c7f4ca1d0cc62784
SakshiI10 <iamsakshiingole@gmail.com>
2024-08-07 23:38:39 +0530

    File added

---
+++ Arrays/36_Max_Pairwise_Computed_Value.py
@@ -0,0 +1,29 @@
+'''
+Given an even sized array arr[], where each consecutive pair of elements represents a height with two fields: feet and inches. Find the maximum height, where the height is calculated by converting feet to inches and adding the inches.
+
+Input: arr[] = [1, 2, 2, 1] 
+Output: 25'''
+
+class Solution:
+    def findMax(self, arr):
+        max_height = 0
+        
+        for i in range(0, len(arr), 2):
+            feet = arr[i]
+            inches = arr[i + 1]
+            
+            total_inches = feet * 12 + inches
+            
+            if total_inches > max_height:
+                max_height = total_inches
+        
+        return max_height
+
+
+sol = Solution()
+    
+arr1 = [1, 2, 2, 1]
+arr2 = [3, 2, 2, 3, 1, 2]
+    
+print(sol.findMax(arr1))  # Expected Output: 25
+print(sol.findMax(arr2))  # Expected Output: 38
