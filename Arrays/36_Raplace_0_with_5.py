6aa92847e73255da36fd82c0725522db10baac3d
SakshiI10 <iamsakshiingole@gmail.com>
2024-08-07 23:21:18 +0530

    File added

---
+++ Arrays/36_Raplace_0_with_5.py
@@ -0,0 +1,25 @@
+'''You are given an integer n. You need to convert all zeroes of n to 5.
+
+Input: n = 1004
+Output: 1554 '''
+
+class Solution:
+    def convertFive(self, n):
+        digits = list(str(n))
+        found_zero = False
+        
+        for i in range(len(digits)):
+            if digits[i] == '0':
+                digits[i] = '5'
+                found_zero = True
+        
+        if found_zero:
+            return int(''.join(digits))
+        else:
+            return n
+
+sol = Solution()
+print(sol.convertFive(1004))  # Output: 1554
+print(sol.convertFive(121))   # Output: 121
+print(sol.convertFive(123))   # Output: 123
+print(sol.convertFive(5))     # Output: 5
