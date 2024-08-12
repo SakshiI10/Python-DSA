61df893d57165fd3387e2aef107995e1882b8e4a
SakshiI10 <iamsakshiingole@gmail.com>
2024-08-12 10:39:24 +0530

    File added

---
+++ Arrays/37_Ishaan_Loves_Chocolates.py
@@ -0,0 +1,26 @@
+'''
+As we know, Ishaan has a love for chocolates. He has bought a huge chocolate bar that contains N chocolate squares. Each of the squares has a tastiness level which is denoted by an array A[].
+Ishaan can eat the first or the last square of the chocolate at once. Ishaan has a sister who loves chocolates too and she demands the last chocolate square. Now, Ishaan being greedy eats the more tasty square first. 
+Determine the tastiness level of the square which his sister gets.
+
+Input : arr[ ] = {5, 3, 1, 6, 9}
+Output : 1'''
+
+from typing import List
+
+class Solution:
+    def chocolates(self, n : int, arr : List[int]) -> int:
+        left, right = 0, n - 1
+        
+        while left < right:
+            if arr[left] >= arr[right]:
+                left += 1
+            else:
+                right -= 1
+                
+        return arr[left]
+
+sol = Solution()
+arr = [5, 3, 1, 6, 9]
+result = sol.chocolates(len(arr), arr)
+print(result)  
--- Arrays/37_Rotate_Array_by_one.py
+++
@@ -1,6 +0,0 @@
-'''
-Given an array arr, rotate the array by one position in clock-wise direction.
-
-Input: arr = [1, 2, 3, 4, 5]
-Output: [5, 1, 2, 3, 4]'''
-
