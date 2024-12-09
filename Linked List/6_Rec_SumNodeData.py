52f96a87fd98a01ee2e59af4a048005bc392b528
SakshiI10 <iamsakshiingole@gmail.com>
2024-12-09 10:19:37 +0530

    6th File added

---
+++ Linked List/6_Rec_SumNodeData.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, data):
+        # Initialize a node with the given data and set the next pointer to None
+        self.data = data        #attributes of the Node class
+        self.next = None        #attributes of the Node class
+
+class LinkedList:
+    def __init__(self):
+        # Initialize the linked list with an empty head
+        self.head = None        #pointer
+
+def Rec_sumNode(head):
+    if head is None:
+        return 0
+    return head.data + Rec_sumNode(head.next)
+
+ll = LinkedList()
+ll.head = Node(1)
+ll.head.next = Node(2)
+ll.head.next.next = Node(3)
+ll.head.next.next.next = Node(4)
+
+print("Sum of Nodes: ",Rec_sumNode(ll.head))
