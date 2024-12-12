a62c9bfaca40ac4fc6947c511cb0641ff27461d1
SakshiI10 <iamsakshiingole@gmail.com>
2024-12-12 12:02:52 +0530

    File added

---
+++ Linked List/13_CLL_Print.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def printList(head):
+    if head is None:
+        print("EMPTY LIST")
+        return
+    
+    # Start from the head node
+    temp = head
+    while True:
+        print(temp.data, end=" ")
+        temp = temp.next
+        if temp == head:  # Stop when we loop back to the head
+            break
+    print()
+
+# Directly initialize the nodes
+head = Node(1)  # First node (head)
+second = Node(2)
+third = Node(3)
+fourth = Node(4)
+
+# Connect the nodes to form a circular linked list
+head.next = second
+second.next = third
+third.next = fourth
+fourth.next = head  # Link the last node back to the head to make it circular
+
+# Print the circular linked list
+printList(head)
