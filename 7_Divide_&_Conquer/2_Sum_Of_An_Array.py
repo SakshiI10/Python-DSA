f0e2a6867086d6fb801d8025fabab82e4039670e
SakshiI10 <iamsakshiingole@gmail.com>
2025-01-01 18:57:35 +0530

    2nd File added

---
+++ 7_Divide_&_Conquer/2_Sum_Of_An_Array.py
@@ -0,0 +1,19 @@
+def find_sum(arr, low, high):
+    # Base case: If the array contains only one element
+    if low == high:
+        return arr[low]
+    
+    # Find the middle of the array
+    mid = (low + high) // 2
+    
+    # Recursively find the maximum in the left half and right half
+    left_sum = find_sum(arr, low, mid)
+    right_sum = find_sum(arr, mid + 1, high)
+    
+    # Return the larger of the two
+    return left_sum + right_sum
+
+if __name__ == "__main__":
+    arr = [3, 5, 1, 9, 2, 8]
+    result = find_sum(arr, 0, len(arr) - 1)
+    print("Sum of elements:", result)
