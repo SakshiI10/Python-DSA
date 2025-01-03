4d2f0b0e0c20d88ec82ace2274bcf9a75c9484c3
SakshiI10 <iamsakshiingole@gmail.com>
2025-01-03 22:21:37 +0530

    1st File added

---
+++ 7_Divide_&_Conquer/5_Merge_Sort.py
@@ -0,0 +1,40 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        # Finding the middle of the array
+        mid = len(arr) // 2
+        
+        # Dividing the array elements into 2 halves
+        L = arr[:mid]
+        R = arr[mid:]
+        
+        # Recursive call on each half
+        merge_sort(L)
+        merge_sort(R)
+        
+        i = j = k = 0
+        
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+        
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+arr = [12, 11, 13, 5, 6, 7]
+print("Original Array: ",arr)
+merge_sort(arr)
+print("Sorted array:", arr)
