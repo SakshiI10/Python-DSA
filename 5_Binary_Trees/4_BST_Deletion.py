1e303ed6638e71f567071c520386717245329b3e
SakshiI10 <iamsakshiingole@gmail.com>
2025-01-28 13:02:36 +0530

    File edited

--- 5_Binary_Trees/4_BST_Deletion.py
+++ 5_Binary_Trees/4_BST_Deletion.py
@@ -32,24 +32,32 @@ def deleteNode(root, key):
         return root
 
     # Traverse the tree to find the node to be deleted
+    # If the key is smaller, go to the left subtree
     if key < root.val:
-        root.left = deleteNode(root.left, key)
+        root.left = deleteNode(root.left, key)  
+    # If the key is larger, go to the right subtree
     elif key > root.val:
-        root.right = deleteNode(root.right, key)
+        root.right = deleteNode(root.right, key) 
     else:
-        # Node with only one child or no child
-        if root.left is None:
+        # Node with only one child or no child (case for 0 or 1 child)
+        # Case 1: No children
+        if root.left is None and root.right is None:
+            return None
+        # Case 2: Only right child
+        elif root.left is None:
             return root.right
+        # Case 3: Only left child
         elif root.right is None:
             return root.left
 
-        # Node with two children: Get the inorder successor (smallest in the right subtree)
+        # Node with two children (case for 2 children):
+        # Get the inorder successor (smallest in the right subtree) or 2 way is to find lergest in left subtree
         temp = minValueNode(root.right)
 
-        # Copy the inorder successor's value to this node
+        # Copy the inorder successor's value to this node (root node now holds the smallest value)
         root.val = temp.val
 
-        # Delete the inorder successor
+        # Delete the inorder successor (since its value is now copied to the root)
         root.right = deleteNode(root.right, temp.val)
 
     return root
