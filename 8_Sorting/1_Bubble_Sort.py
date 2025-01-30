b89e1f88e78102b48fe8aead5aac4a430dc85852
SakshiI10 <iamsakshiingole@gmail.com>
2025-01-30 18:47:25 +0530

    File added

---
+++ 8_Sorting/1_Bubble_Sort.py
@@ -0,0 +1,20 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j] 
+                swapped = True
+        if not swapped:  # If no swaps happened, the array is already sorted
+            break
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+print("Sorted array:", arr)
+
+# Time Complexities:
+# Best: O(n)
+# Average: O(n^2) 
+# Worst: O(n^2)
+# Space: O(1)
