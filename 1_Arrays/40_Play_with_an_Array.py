5a432a2f757044107726424ee9168ebc50b94357
SakshiI10 <iamsakshiingole@gmail.com>
2025-02-23 19:01:47 +0530

    File edited

--- 1_Arrays/40_Play_with_an_Array.py
+++ 1_Arrays/40_Play_with_an_Array.py
@@ -12,9 +12,6 @@ class Solution:
         for i in range(1, n, 2):
             if arr[i] <= arr[i - 1]:
                 arr[i], arr[i - 1] = arr[i - 1], arr[i]
-                
-        for i in range(1, n, 2):
-            if arr[i] <= arr[i - 1]:
                 return 'false'
         return 'true'
 
