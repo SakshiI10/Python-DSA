65e68e322bfa561fac10dec30aa43f7259ec4672
SakshiI10 <iamsakshiingole@gmail.com>
2025-02-26 23:26:35 +0530

    File added

--- 1_Arrays/31_Rotation.py
+++ 1_Arrays/31_Rotation.py
@@ -3,22 +3,23 @@ Given an array arr, rotate the array by one position in clock-wise direction.
 Input: arr = [1, 2, 3, 4, 5]
 Output: [5, 1, 2, 3, 4]'''
 
-def left_rotate(arr, d):
-    n = len(arr)
-    d = d % n  
-    return arr[d:] + arr[:d]
+class Solution:
+    def left_rotate(self, arr, d):
+        n = len(arr)
+        d = d % n  
+        return arr[d:] + arr[:d]
 
-def right_rotate(arr, d):
-    n = len(arr)
-    d = d % n  
-    return arr[-d:] + arr[:-d]
+    def right_rotate(self, arr, d):
+        n = len(arr)
+        d = d % n  
+        return arr[-d:] + arr[:-d]
 
+sol=Solution()
 arr = [9, 8, 7, 6, 4, 2, 1, 3]
 d = 1
-
-left_rotated_arr = left_rotate(arr, d)
-right_rotated_arr = right_rotate(arr, d)
+sol.left_rotate(arr, d)
+sol.right_rotate(arr, d)
 
 print(f"Original array: {arr}")
-print(f"Array after left rotation by {d}: {left_rotated_arr}")
-print(f"Array after right rotation by {d}: {right_rotated_arr}")
+print(f"Array after left rotation by {d}: {sol.left_rotate(arr, d)}")
+print(f"Array after right rotation by {d}: {sol.right_rotate(arr, d)}")
