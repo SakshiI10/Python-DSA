b1ec504511907744916f4e81eed166a51d97b887
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-02 19:44:17 +0530

    1 file added

--- 0_WarmUp_Mastering_Loops/1_Star_Pattern.py
+++ 0_WarmUp_Mastering_Loops/1_Star_Pattern.py
@@ -6,5 +6,15 @@
 ***** '''
 
 n=5
+
+print("Using one loop: ")
+# Using one loop
 for i in range(n):
-    print('*'*n)
+    print("*" * n)  # Print 5 stars in a single row
+
+print("Using two loops: ")
+# Using two loops
+for i in range(n):
+    for j in range(n):
+        print('*', end="") 
+    print()
