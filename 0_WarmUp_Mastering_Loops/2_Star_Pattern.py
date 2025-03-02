0d0aa793836bdd42ab7bd2723351e81d4065feeb
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-02 19:54:59 +0530

    2nd file added

--- 0_WarmUp_Mastering_Loops/2_Star_Pattern.py
+++ 0_WarmUp_Mastering_Loops/2_Star_Pattern.py
@@ -5,6 +5,16 @@
 ****
 ***** '''
 
+def pattern_using_one_loop(n):
+    for i in range(n+1):
+        print("*"*i)
+
+def pattern_using_two_loops(n):
+    for i in range(n+1):
+        for j in range(i):
+            print('*', end="")
+        print()
+
 n=5
-for i in range(n+1):
-    print('*' * i)
+pattern_using_one_loop(n)
+pattern_using_two_loops(n)
