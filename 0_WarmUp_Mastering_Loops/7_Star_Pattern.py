a2ab83bce3634cd12cc8e1a550ae8443df5b55a3
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-03 13:20:33 +0530

    file added

--- 0_WarmUp_Mastering_Loops/7_Star_Pattern.py
+++ 0_WarmUp_Mastering_Loops/7_Star_Pattern.py
@@ -5,6 +5,18 @@
    ***
     * '''
 
-n = 5
-for i in range(n):
-    print(' ' * i + '*' * (2*(n-i)-1))
+def pattern_using_single_loop(n):
+    for i in range(n):
+        print(' '*i + '*'*(2*(n-i)-1))
+
+def pattern_using_nested_loops(n):
+    for i in range(n):
+        for j in range(i):
+            print(' ', end='') 
+        for j in range(2*(n-i)-1):
+            print('*', end='')  
+        print()
+
+n=5
+pattern_using_single_loop(n)
+pattern_using_nested_loops(n)
