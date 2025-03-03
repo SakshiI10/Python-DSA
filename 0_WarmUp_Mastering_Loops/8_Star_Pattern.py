a1c965ca56ddc14911a05e80d180799f2e881011
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-03 13:34:23 +0530

    file added

--- 0_WarmUp_Mastering_Loops/8_Star_Pattern.py
+++ 0_WarmUp_Mastering_Loops/8_Star_Pattern.py
@@ -7,9 +7,10 @@
 
 def hollow_square(n):
     for i in range(n):
-        if i == 0 or i == n - 1:
-            print('*' * n)
+        if(i==0 or i==(n-1)):
+            print('*'*n)
         else:
-            print('*' + ' ' * (n - 2) + '*')
+            print('*'+' '*(n-2)+'*')
 
-hollow_square(5)
+n=5
+hollow_square(n)
