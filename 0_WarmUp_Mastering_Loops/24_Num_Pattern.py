1cd1081cd4a6f96aaa7757f71651fcdfd43062a4
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-05 18:56:04 +0530

    file added

--- 0_WarmUp_Mastering_Loops/24_Num_Pattern.py
+++ 0_WarmUp_Mastering_Loops/24_Num_Pattern.py
@@ -6,3 +6,13 @@
 4 3 2 2 2 3 4
 4 3 3 3 3 3 4
 4 4 4 4 4 4 4'''
+
+def print_pattern(n):
+    for i in range(n):
+        for j in range(n):
+            value = (n // 2) - min(i, j, n - i - 1, n - j - 1) + 1
+            print(value, end=" ")
+        print()  
+
+n = 7
+print_pattern(n)
