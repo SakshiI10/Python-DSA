50e946e2e91af27cb13585eeefc53b357e8db630
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-09 12:56:53 +0530

    file added

---
+++ 2_BasicMaths_And_Recursion/13_Rec_Functional_Palindrome.py
@@ -0,0 +1,9 @@
+def palindrome(i, s):
+    if i >= len(s) // 2:
+        return True
+    if s[i] != s[len(s) - i - 1]:  
+        return False
+    return palindrome(i + 1, s)  
+
+s = "madam"
+print(palindrome(0, s))
