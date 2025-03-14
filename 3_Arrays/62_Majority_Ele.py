73a874649026da9cb6c4bb5684702c3725cae085
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-14 13:59:15 +0530

    file added

---
+++ 3_Arrays/62_Majority_Ele.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majority(self, arr):
+        #Brute Force
+        n=len(arr)
+        for i in range(n):
+            count=0
+            for j in range(n):
+                if (arr[i]==arr[j]):
+                    count += 1
+        if count>n//2:
+            return arr[i]
+        return -1
+            
+sol=Solution()
+arr=[2, 2, 3, 3, 1, 2, 2]
+print(sol.majority(arr))
+
+#Not efficient, so did it using dictionary (present in hashing folder)
---
+++ 8_Hashing/5_Majority_Ele.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majority(self, arr):
+        hash_table={}
+
+        for num in arr:
+            if num in hash_table:
+                hash_table[num] += 1
+            else:
+                hash_table[num]=1
+        for key, value in hash_table.items():
+            if value > n//2:
+                return key
+        return -1
+            
+sol=Solution()
+arr=[2, 2, 3, 3, 1, 2, 2]
+print(sol.majority(arr))
