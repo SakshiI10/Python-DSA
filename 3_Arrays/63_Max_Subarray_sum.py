31c53480848d9754f359fa5b8b540626b91c6677
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-14 18:13:19 +0530

    file added

---
+++ 3_Arrays/63_Max_Subarray_sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def max_subarray(self, arr):
+        #Brute Force
+        n=len(arr)
+        max_sum=float('-inf')
+
+        for i in range(n):
+            sum=0
+            for j in range(i, n):
+                sum += arr[j]
+                max_sum=max(max_sum, sum)
+        return max_sum
+
+sol=Solution()
+arr=[-2, -3, 4, -1, -2, 1, 5, -3]
+print(sol.max_subarray(arr))
