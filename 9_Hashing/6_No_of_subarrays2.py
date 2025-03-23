c55c59dbb977bdb94cc7b16a0706611a483447a9
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-24 00:02:31 +0530

    file added

--- 9_Hashing/6_No_of_subarrays2.py
+++ 9_Hashing/6_No_of_subarrays2.py
@@ -6,8 +6,12 @@ class Solution:
 
         for num in arr:
             prefix_sum += num
-            if prefix_sum-k in hash_map
-        
+            if prefix_sum-k in hash_map:
+                # # Add occurrences of (prefix_sum - k)
+                count += hash_map[prefix_sum-k]
+            # Store/update the count of this prefix sum
+            hash_map[prefix_sum] = hash_map.get(prefix_sum, 0) + 1
+        return count
     
 sol=Solution()
 arr=[1, 2, 3, -3, 1, 1, 1, 4, 2, -3]
