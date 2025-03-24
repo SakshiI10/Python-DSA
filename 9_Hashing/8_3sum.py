5992269251e404938310063d875df2e47e8940e7
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-25 00:32:32 +0530

    file edited

---
+++ 9_Hashing/8_3sum.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution():
+    def threesum(self, arr: List[int], target: int):
+        # arr.sort()
+        n=len(arr)
+        res = set()
+
+        for i in range(n):
+            hash_table = set()  
+            for j in range(i + 1, n):
+                complement = target - (arr[i] + arr[j])
+                if complement in hash_table:
+                    triplet = (arr[i], arr[j], complement)
+                    res.add(tuple(sorted(triplet)))  
+                hash_table.add(arr[j])
+
+        return [list(triplet) for triplet in res] if res else "No"
+                
+sol=Solution()
+a = [-1, 0, 1, 2, -1, -4]
+k = 0
+len1 = sol.threesum(a, k)
+print(len1)
