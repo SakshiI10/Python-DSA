e729f3aff09050df7765c827960ff2eddd9be625
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-24 12:38:55 +0530

    file added

--- 3_Arrays/71_Majority_Ele2.py
+++ 3_Arrays/71_Majority_Ele2.py
@@ -2,15 +2,15 @@ class Solution:
     def majority(self, arr):
         #Brute Force
         n=len(arr)
-        ans=[]
+        res=[]
         for i in range(n):
             count=0
             for j in range(n):
                 if (arr[i]==arr[j]):
                     count += 1
-            if count>n//3 and arr[i] not in ans:
-                ans.append(arr[i])
-        return ans if ans else -1
+            if count>n//3 and arr[i] not in res:
+                res.append(arr[i])
+        return res if res else -1
             
 sol=Solution()
 arr=[1, 1, 1, 3, 3, 2, 2, 2]
--- 9_Hashing/7_Majority_Ele2.py
+++ 9_Hashing/7_Majority_Ele2.py
@@ -2,17 +2,17 @@ class Solution:
     def majority(self, arr):
         hash_table={}
         n=len(arr)
-
+        res=[]
         for num in arr:
             if num in hash_table:
                 hash_table[num] += 1
             else:
                 hash_table[num]=1
         for key, value in hash_table.items():
-            if value > n//2:
-                return key
-        return -1
+            if value > n//3:
+                res.append(key)
+        return res if res else -1
             
 sol=Solution()
-arr=[2, 2, 3, 3, 1, 2, 2]
+arr=[1, 1, 1, 3, 3, 2, 2, 2]
 print(sol.majority(arr))
