900428977b5280a6de02252eb6606585f1233116
SakshiI10 <iamsakshiingole@gmail.com>
2025-03-31 11:52:52 +0530

    file added

---
+++ 13_Search/2_Lower_Upper_Bound.py
@@ -0,0 +1,34 @@
+class Solution:
+    def lower_bound(self, arr, target, n):
+        low, high = 0, n-1
+        ans = target
+
+        while(low<=high):
+            mid=(low+high)//2
+            if arr[mid] >= target:
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ans
+
+    def upper_bound(elf, arr, target, n):
+        low, high = 0, n-1
+        ans = target
+
+        while(low<=high):
+            mid=(low+high)//2
+            if arr[mid] >= target:
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ans
+
+sol=Solution()
+arr=[1, 2, 3, 3, 5, 8, 8, 10, 11]
+n=len(arr)
+target=9
+print(sol.lower_bound(arr, target, n))
+target=6
+print(sol.upper_bound(arr, target, n))
