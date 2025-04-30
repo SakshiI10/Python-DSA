dcfbcc25c4b221cbe79cd77e7c389488b3c21d02
SakshiI10 <iamsakshiingole@gmail.com>
2025-04-30 10:07:40 +0530

    file edited

--- 3_Arrays/95_Last_index_of_1.py
+++ 3_Arrays/95_Last_index_of_1.py
@@ -5,6 +5,13 @@ Input: s = 00001
 Output: 4'''
 
 class Solution:
+    def firstIndex(self, arr):
+        n=len(arr)
+        for i in range(n):
+            if arr[i]==1:
+                return i
+        return -1
+    
     def lastIndex(self, s):
         n=len(s)
         
@@ -12,6 +19,9 @@ class Solution:
             if s[i]=='1':
                 return i
         return -1
+    
 sol=Solution()
+arr=[0, 0, 0, 0, 0, 0, 1, 1, 1, 1]
 s='00001'
+print(sol.firstIndex(arr))
 print(sol.lastIndex(s))
