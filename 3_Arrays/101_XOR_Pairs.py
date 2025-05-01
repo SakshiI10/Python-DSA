6e39d1513dec20f64a4168aa5a80b61660d72f7a
SakshiI10 <iamsakshiingole@gmail.com>
2025-05-01 12:02:56 +0530

    file added

---
+++ 3_Arrays/101_XOR_Pairs.py
@@ -0,0 +1,26 @@
+'''
+Given an array arr[] of integers, find the number of pairs whose XOR is odd in the array.
+
+Input: arr[] = [1, 2, 3]
+Output: 2
+
+Input: arr[] = [1, 2]
+Output: 1'''
+
+class Solution:
+
+    def countOddXorPairs(self, arr):
+        odd_count = 0
+        even_count = 0
+
+        for num in arr:
+            if num % 2 == 0:
+                even_count += 1
+            else:
+                odd_count += 1
+        
+        return odd_count * even_count
+    
+sol=Solution()
+arr=[1, 2, 3]
+print(sol.countOddXorPairs(arr))
