4f4f87b76ba62826bd0ad11dd58d41a4e05225a4
SakshiI10 <iamsakshiingole@gmail.com>
2025-05-22 20:30:32 +0530

    file added

---
+++ 3_Arrays/111_Fibonacci_in_the_series.py
@@ -0,0 +1,29 @@
+'''
+Given an array arr of integers, the task is to count the number of elements of the array which are Fibonacci numbers
+
+Input: arr[] = [4, 2, 8, 5, 20, 1, 40, 13, 23]
+Output: 5'''
+
+class Solution:
+    def countFibonacciNumbers(self, arr):
+        if not arr:
+            return 0
+        
+        max_elem = max(arr)
+        
+        fib_set = set()
+        a, b = 0, 1
+        while a <= max_elem:
+            fib_set.add(a)
+            a, b = b, a + b
+        
+        count = 0
+        for num in arr:
+            if num in fib_set:
+                count += 1
+        
+        return count
+
+sol = Solution()
+arr = [4, 2, 8, 5, 20, 1, 40, 13, 23]
+print(sol.countFibonacciNumbers(arr))
