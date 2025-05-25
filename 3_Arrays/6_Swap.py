51248dcd9aad27e1cef715b86894396e6b51fda2
SakshiI10 <iamsakshiingole@gmail.com>
2025-05-25 21:54:16 +0530

    file added

--- 3_Arrays/6_Swap.py
+++ 3_Arrays/6_Swap.py
@@ -7,9 +7,8 @@ k = 3
 arr[] = {1, 2, 3, 4, 5, 6, 7, 8}
 Output: {1, 2, 6, 4, 5, 3, 7, 8}'''
 
-from typing import List
 class Solution:
-    def swapKth(self, n : int, k : int, arr : List[int]) -> None:
+    def swapKth(self, n, k, arr):
         start_index = k-1
         end_index = n-k
         arr[start_index], arr[end_index] = arr[end_index], arr[start_index]
