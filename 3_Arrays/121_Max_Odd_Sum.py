e719ba84486ac82b319db9315da0ff7c98766feb
SakshiI10 <iamsakshiingole@gmail.com>
2025-05-28 10:46:34 +0530

    file added

---
+++ 3_Arrays/121_Max_Odd_Sum.py
@@ -0,0 +1,32 @@
+'''
+Given an array arr[] of integers, determine whether a subsequence exists with an odd sum. If such a subsequence exists, return the maximum possible odd sum. If no subsequence with an odd sum can be formed, return -1.
+
+Input: arr = [4, -3, 3, -5]
+Output: 7
+
+Input: arr = [2, 5, -4, 3, -1]
+Output: 9'''
+
+class Solution:
+    def findMaxOddSum(self, arr):
+        min_odd = float('inf')
+        isOdd = False
+        sum = 0
+
+        for num in arr:
+            if num > 0:
+                sum += num
+            if num % 2 != 0:
+                isOdd = True
+                if min_odd > abs(num):
+                    min_odd = abs(num)
+
+        if not isOdd:
+            return -1
+        if sum % 2 == 0:
+            sum -= min_odd
+        return sum
+
+sol=Solution()
+arr=[4, -3, 3, -5]
+print(sol.findMaxOddSum(arr))  
