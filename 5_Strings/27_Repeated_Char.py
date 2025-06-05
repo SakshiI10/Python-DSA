4c295ad5de9acf65365fbddd6d9856b5d85b2c40
SakshiI10 <iamsakshiingole@gmail.com>
2025-06-05 11:40:51 +0530

    file edited

--- 5_Strings/27_Repeated_Char.py
+++ 5_Strings/27_Repeated_Char.py
@@ -4,22 +4,41 @@ Given a string consisting of lowercase english alphabets. Find the repeated char
 Input: S = "geeksforgeeks"
 Output: g'''
 
+# Returns character
+# class Solution:
+#     def firstRep(self, s):
+#         count_dict = {} 
+        
+#         for char in s:
+#             if char in count_dict:
+#                 count_dict[char] += 1
+#             else:
+#                 count_dict[char] = 1
+        
+#         for char in s:
+#             if count_dict[char] > 1:
+#                 return char
+        
+#         return '#'
+
+# Returns index
 class Solution:
-    def firstRep(self, s):
-        count_dict = {}  # Dictionary to keep track of character counts
+    def repeatingCharacter(self,s):
+        freq = {} 
         
         for char in s:
-            if char in count_dict:
-                count_dict[char] += 1
+            if char in freq:
+                freq[char] += 1
             else:
-                count_dict[char] = 1
+                freq[char] = 1
         
-        # Iterate again to find the first character with a count greater than 1
+        index = 0
         for char in s:
-            if count_dict[char] > 1:
-                return char
-        
-        return '#'  # Return '#' if no repeated characters are found
+            if freq[char] > 1:
+                return index 
+            index += 1
+
+        return -1
 
 sol = Solution()
 print(sol.firstRep("geeksforgeeks"))  # Output: g
