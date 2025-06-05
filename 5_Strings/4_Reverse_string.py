553a6667bc9cd7adee2df1e6dd3de04f53afb3e9
SakshiI10 <iamsakshiingole@gmail.com>
2025-06-05 11:59:29 +0530

    file edited

--- 5_Strings/4_Reverse_string.py
+++ 5_Strings/4_Reverse_string.py
@@ -12,3 +12,19 @@ class Solution:
 sol = Solution()
 str = "qwerty"
 print(sol.revStr(str))
+
+'''
+Given two strings S1 and S2 as input. Your task is to concatenate two strings and then reverse the string. Finally print the reversed string.
+Input: S1 = "Geeks" , S2 = "forGeeks"
+Output: "skeeGrofskeeG" 
+'''
+class Solution:
+    def conRevstr (ob, S1, S2):
+        combined = S1 + S2
+        rev_str=combined[::-1]
+        return rev_str
+
+sol = Solution()
+S1= "Geeks"
+S2= "forGeeks"  
+print(sol.conRevstr(S1, S2))
