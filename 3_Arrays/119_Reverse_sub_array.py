2ce14c6f55e5a2eb4cfd6a5f17f02c97246c98cc
SakshiI10 <iamsakshiingole@gmail.com>
2025-06-20 12:07:42 +0530

    file edited

--- 3_Arrays/119_Reverse_sub_array.py
+++ 3_Arrays/119_Reverse_sub_array.py
@@ -1,8 +1,6 @@
 '''
 Given an array arr, you need to reverse a subarray of that array. The range of this subarray is given by indices l and r (1-based indexing).
 
-Examples:
-
 Input: arr[] = [1, 2, 3, 4, 5, 6, 7], l = 2, r = 4
 Output: [1, 4, 3, 2, 5, 6, 7]'''
 
