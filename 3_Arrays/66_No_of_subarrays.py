1a34581201517d9a55d4649d0677495fe4172220
SakshiI10 <iamsakshiingole@gmail.com>
2025-06-25 11:13:40 +0530

    file edited

--- 3_Arrays/66_No_of_subarrays.py
+++ 3_Arrays/66_No_of_subarrays.py
@@ -1,15 +1,23 @@
+# This will return count as well as the numbers.
+
 class Solution:
     #Brute Force
     def max_subarray(self, arr, k):
         n=len(arr)
         count=0
+        res=[]
+
         for i in range(n):
             sum=0
+            temp=[]
             for j in range(i, n):
                 sum += arr[j]
+                temp.append(arr[j])
+
                 if sum == k:
                     count += 1
-        return count
+                    res.append(temp[:])
+        return count, res
     
 sol=Solution()
 arr=[1, 2, 3, -3, 1, 1, 1, 4, 2, -3]
