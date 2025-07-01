276ef8ebdce5fb31bd324eccab5c741e0fec0d22
SakshiI10 <iamsakshiingole@gmail.com>
2025-07-01 22:57:55 +0530

    file added

--- 5_Strings/78_Check_Binary.py
+++ 5_Strings/78_Check_Binary.py
@@ -7,9 +7,11 @@ Output: true'''
 class Solution:
     def isBinary(self, str):
         cnt = 0
+
         for i in str:
             if i == '0' or i == '1':
                 cnt += 1
+                
         if cnt == len(str):
             return True
         return False
