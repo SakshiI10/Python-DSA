28e48f0234477cdd46b8b2df60eca2e7bb8bfa2a
SakshiI10 <iamsakshiingole@gmail.com>
2025-07-28 13:59:05 +0530

    file added

--- 9_Hashing/3_2sum.py
+++ 9_Hashing/3_2sum.py
@@ -1,19 +1,35 @@
 class Solution():
     def twosum(self, arr, k):
-        hash_table={}
         n=len(arr)
+        hash_table={}
 
         for i in range(n):
             compliment=k-arr[i]
             if compliment in hash_table:
                 return[hash_table[compliment], i]
             hash_table[arr[i]]=i
+
+    def twosum2(self, arr, k):
+        n = len(arr)
+        hash_table = {}
+        result = []
+
+        for i in range(n):
+            complement = k - arr[i]
+            if complement in hash_table:
+                for j in hash_table[complement]:
+                    result.append([j, i])
+
+            if arr[i] not in hash_table:
+                hash_table[arr[i]] = []
+            hash_table[arr[i]].append(i)
+
+        return sorted(result)
                        
 sol=Solution()
-a = [2, 6, 5, 8, 11]
-k = 10
-len1 = sol.twosum(a, k)
-print(len1)
+a = [10, 20, 30, 20, 10, 30]
+k = 50
+print(sol.twosum(a, k))
+print(sol.twosum2(a, k))
 
 # hash_table[compliment] is returning a list containing two indices: the index of the complement number (which was seen earlier and stored in the hash table), i — the index of the current number being checked.
-
